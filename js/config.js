6c2c79b8e43b0906314440c1bae8222d352195be
Raul Herrera <raulch626@gmail.com>
2025-04-01 14:30:39 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_l3dZAdEBf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1k9qubadjd7uc3cdkicuhegcbs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
